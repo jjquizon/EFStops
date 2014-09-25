ea25d81e16fd3818d03f694cf6d88007a8cf4279
Jeffrey Quizon <jeffrey.quizon@gmail.com>
2014-09-25 05:58:50 -0700

    Change application files

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module EfStops
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.filepicker_rails.api_key = <%= ENV["filepicker_api_key"] %>
+    # config.filepicker_rails.api_key = <%= ENV["filepicker_api_key"] %>
     config.assets.paths << "app/assets/templates"
   end
 end
